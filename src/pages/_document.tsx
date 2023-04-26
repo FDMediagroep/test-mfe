22be784abeab369dfceecdd7524e6e2fd3554689
Willem Liu <willem.liu@fdmediagroep.nl>
2023-04-26 16:23:05 +0200

    First commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://static.fd.nl/assets/5.13.5/css/fd.fonts.css"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
